acd22eaa0c3f7d640a8e776ae5e2ed2340396849
Christian Nilsson <christian@klamby.com>
2012-11-02 15:55:51 +0100

    More javascript

--- source/javascripts/custom.js
+++ source/javascripts/custom.js
@@ -1,13 +1,44 @@
 window.addEvent('domready', function() {
-	// Sidebar "About me"
-	var link = $("about-me-sidebar").getElements('a')[0];
+	// Settings
 	var settings = {
-		show: "&larr; Läs mer om mig!",
-		hide: "&larr; Läs mindre om mig!"
+		// Link
+		show: '&larr; Läs mer om mig!',
+		hide: '&larr; Läs mindre om mig!',
+
+		// Ajax
+		url: '/om-christian-nilsson',
+		load: 'Laddar...',
+		fail: "Fail! :'("
 	}
 
+	// Sidebar "About me"
+	var link = $("about-me-sidebar").getElements('a')[0];
+	var sidebar = $("about-me");
+
+	// Load text for sidebar
+	var req = new Request.HTML({
+		url: settings.url,
+		method: 'get',
+		onRequest: function() {
+			// Loading...
+			sidebar.set('html', settings.load);
+		},
+		onSuccess: function(data, elements) {
+			// SUCCESS!!!11oneone
+			sidebar.set('html', elements.getElement('article')[0].innerHTML);
+			window.ch = elements;
+		},
+		onFailure: function() {
+			// Fail! :'(
+			sidebar.set('html', settings.fail);
+		}
+	});
+	req.send();
+
+	// Set link-text
 	link.set('html', settings.show);
 
+	// Click on link
 	link.addEvent("click", function(event) {
 		// Prevent default
 		event.stop();
