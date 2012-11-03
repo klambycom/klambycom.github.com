6634d3f78edeb79eb5cdf78fec867de17974143a
Christian Nilsson <christian@klamby.com>
2012-11-03 01:14:30 +0100

    Site updated at 2012-11-03 00:14:30 UTC

--- javascripts/custom.js
+++ javascripts/custom.js
@@ -1,62 +1,66 @@
 window.addEvent('domready', function() {
-	var settings = {
-		// Link
-		show: '&larr; Läs mer om mig!',
-		hide: '&larr; Läs mindre om mig!',
-
-		// Ajax
-		url: '/om-christian-nilsson',
-		load: 'Laddar...',
-		fail: "Fail! :'("
-	};
-
 	// Only desktop
 	if (!(Browser.Platform.android || Browser.Platform.ios)) {
-		aboutMe(settings);
+		new AboutMe({
+			// Link
+			show: '&larr; Läs mer om mig!',
+			hide: '&larr; Läs mindre om mig!',
+
+			// Ajax
+			url: '/om-christian-nilsson',
+			load: 'Laddar...',
+			fail: "Fail! :'("
+		}).run();
 	}
 });
 
-var aboutMe = function(settings) {
-	// Sidebar "About me"
-	var link = $("about-me-sidebar").getElements('a')[0];
-	var sidebar = $("about-me");
-	var loaded = false;
+function AboutMe(settings) {
+	this.link = $("about-me-sidebar").getElements('a')[0];
+	this.sidebar = $("about-me");
+	this.loaded = false;
+	this.settings = settings;
+}
 
-	// Function that load text for sidebar
-	var req = function() {
-		new Request.HTML({
-			url: settings.url,
-			method: 'get',
-			onRequest: function() {
-				// Loading...
-				sidebar.set('html', settings.load);
-			},
-			onSuccess: function(data, elements) {
-				// SUCCESS!!!11oneone
-				sidebar.set('html', elements.getElement('article')[0].innerHTML);
-				loaded = true;
-			},
-			onFailure: function() {
-				// Fail! :'(
-				sidebar.set('html', settings.fail);
-			}
-		}).send();
-	};
+// Function that load text for sidebar
+AboutMe.prototype.request = function() {
+	var self = this;
+
+	new Request.HTML({
+		url: self.settings.url,
+		method: 'get',
+		onRequest: function() {
+			// Loading...
+			self.sidebar.set('html', self.settings.load);
+		},
+		onSuccess: function(data, elements) {
+			// SUCCESS!!!11oneone
+			self.sidebar.set('html', elements.getElement('article')[0].innerHTML);
+			self.loaded = true;
+		},
+		onFailure: function() {
+			// Fail! :'(
+			self.sidebar.set('html', self.settings.fail);
+		}
+	}).send();
+};
+
+AboutMe.prototype.run = function() {
+	var self = this;
 
 	// Set link-text
-	link.set('html', settings.show);
+	this.link.set('html', self.settings.show);
 
 	// Click on link
-	link.addEvent("click", function(event) {
+	this.link.addEvent("click", function(event) {
 		// Prevent default
 		event.stop();
 
 		// Load text for sidebar if it's needed
-		if (!loaded) { req(); }
+		if (!self.loaded) { self.request(); }
 
 		// Change link-text after half transition
 		(function() {
-			this.set('html', ((this.hasClass("flip")) ? settings.hide : settings.show));
+			this.set('html', ((this.hasClass("flip")) ? self.settings.hide : self.settings.show));
 		}).delay(300, this);
 
 		// Mirror link
